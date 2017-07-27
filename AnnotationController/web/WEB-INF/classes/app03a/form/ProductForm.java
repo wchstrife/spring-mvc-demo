d2ae0d66386f2eb571b6e9544f87e1027d471a02
wangchenghao <1021717079@qq.com>
2017-07-27 17:34:53 +0800

    基于注解的controller

---
+++ AnnotationController/web/WEB-INF/classes/app03a/form/ProductForm.java
@@ -0,0 +1,35 @@
+package app03a.form;
+
+/**
+ * Created by wangchenghao on 2017/7/20.
+ */
+public class ProductForm {
+
+    private String name;
+    private String description;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
